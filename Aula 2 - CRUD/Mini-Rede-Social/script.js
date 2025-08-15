0a14435aaf91beb47834d7c9a800d4627991069e
Igor <igornfreitas999@gmail.com>
2025-08-14 22:30:13 -0300

    dasd

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -15,6 +15,70 @@ let posts = [
     {
         text: "Este é o terceiro post teste",
         category: "Eventos",
+        image: "https://placedog.net/150?random=2",
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function() {
+    mostrarPosts();
+
+    document.querySelector("#postForm").addEventListener("submit", addPost); 
+}
+
+//CREATE
+function addPost(infosDoEvento) {
+    infosDoEvento.preventDefault(); // Impede o envio do formulário
+
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+    const dataPost = new Date().toLocaleString(); // Pega a data atual formatada
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: dataPost
+    };
+    posts.unshift(novoPost); // Adiciona o novo post no início do array
+    mostrarPosts(); // Atualiza a lista de posts
+}
+//READ
+function mostrarPosts(){
+    //Pega o elemento da lista de posts
+    const listaPosts = document.querySelector("#postList");
+    listaPosts.innerHTML = ""; // Limpa a lista antes de adicionar novos posts
+
+    posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div");
+        cardPost.classList.add("card-post");
+        cardPost.innerHTML = `
+            <h2>${pegaItem.text}</h2>
+            <img src="${pegaItem.image}"/>
+            <p><strong>Categoria:</strong> ${pegaItem.category}</p>
+            <p><strong>Data:</strong> ${pegaItem.date}</p>
+
+            <div>
+                <button class="btn-editar">Editar</button>
+                <button class="btn-deletar">Deletar</button>
+            </div>
+        `;
+        listaPosts.append(cardPost);
+})
+}
+//UPDATE
+function editarPost(){}
+//DELETE
+function deletarPost(){}
+
+// const pessoa = {
+//     nome: "João",
+//     idade: 30,
+//     Tags: ["amigo", "programador", "gamer"],
+//     "Empresas trabalhadas": ["Empresa A", "Empresa B"],
+//     cidade: "São Paulo"
+// };
+
+// console.log(pessoa.nome);
+// console.log(pessoa[idade])
